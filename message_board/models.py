6e589d8da2d60dad7b532598e251909dff44f56f
unknown <alejandromerlina324.com>
2024-12-16 11:53:27 -0400

    Model added

--- message_board/models.py
+++ message_board/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+
+    title=models.CharField(max_length=200)
+    body=models.TextField()
+
+    class Meta:
+        verbose_name = "Publicacion"
+        verbose_name_plural = "Publicaciones"
+
+    def __str__(self):
+        return self.title
